cbef24642031f7e904f8a1fb4c32ddb8cc6a3e76
blitz <blitz@DESKTOP-7Q18A6N>
2023-01-31 23:18:29 +0000

    Add project files.

---
+++ project/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNum_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            int inputNum = int.Parse((string)btn.Content);
+            TbNumIn.Text = TbNumIn.Text + $"{inputNum}";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TbNumIn.Text = string.Empty;
+        }
+    }
+}
