12e17f7d4c1877f62ec96cefe10a239785e4a429
Matthew Blain <S00211783@mail.itsligo.ie>
2023-02-14 01:59:50 +0000

    Login Set Up

--- project/App.xaml.cs
+++ project/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace project
     /// </summary>
     public partial class App : Application
     {
+      
     }
 }
--- project/Login.xaml.cs
+++ project/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,16 +18,79 @@ namespace project
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Login : Window
     {
-        public Window1()
+        List<User> users = GetUsers();
+
+        private static List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            User u1 = new User(1, "John", 123, true);
+            User u2 = new User(2, "JMary", 123, false);
+            users.Add(u1);
+            users.Add(u2);
+            return users;
+           
+        }
+
+        public Login()
         {
             InitializeComponent();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int inputId = int.Parse(tbxID.Text);
+            int inputPw = int.Parse(pwbPW.Password);
+            string errorMessage = "Sorry Incorrect Id / Pin Entered";
+            //foreach (var user in users)
+            //{
+            //    if(inputId == user.Id)
+            //    {
+            //        if(inputPw == user.Password)
+            //        {
+            //            MainWindow main = new MainWindow();
+            //            main.Show();
+            //            Close();
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show(errorMessage);
+            //            tbxID.Text = String.Empty;
+            //            pwbPW.Password = null;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show(errorMessage);
+            //        tbxID.Text = String.Empty;
+            //        pwbPW.Password = null;
+            //    }
+            //    break;
+            //}
+            var checklogin = from user in users
+                              where inputId == user.Id && inputPw == user.Password
+                              select user;
+            User u = checklogin.FirstOrDefault();
+            if (u != null)
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show(errorMessage);
+                tbxID.Text = String.Empty;
+                pwbPW.Password = null;
+            }
         }
     }
 }
